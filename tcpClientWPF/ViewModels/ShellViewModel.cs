b4a435b14c1c74f30ef1deee6483c72b96f7e65d
Gašper Mauer <gasper.mauer@gmail.com>
2020-03-11 20:54:57 +0100

    Added window controll buttons and colour changes to app

--- tcpClientWPF/ViewModels/ShellViewModel.cs
+++ tcpClientWPF/ViewModels/ShellViewModel.cs
@@ -12,11 +12,48 @@ using System.Windows;
 using System.Threading;
 using AsynchronousSockeClient.Networking;
 using System.Globalization;
+using System.Runtime.InteropServices;
 
 namespace tcpClientWPF.ViewModels
 {
     public class ShellViewModel : Screen
     {
+
+        #region Window Control
+
+        private WindowState windowState;
+        public WindowState WindowState
+        {
+            get { return windowState; }
+            set
+            {
+                windowState = value;
+                NotifyOfPropertyChange(() => WindowState);
+            }
+        }
+
+        public void MaximizeWindow()
+        {
+            if (WindowState == WindowState.Maximized)
+                WindowState = WindowState.Normal;
+            else
+                WindowState = WindowState.Maximized;
+        }
+
+        public void MinimizeWindow()
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        public bool myCondition { get { return (false); } }
+
+        public void CloseWindow()
+        {
+            Application.Current.Shutdown();
+        }
+
+        #endregion
+
         #region Private Members
 
         private Socket _socket;
