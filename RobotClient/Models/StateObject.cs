45bf762b20f64ab2377df0546ed934e1cba41523
Vid Cresnik <vcresnik@gmail.com>
2020-04-13 22:29:02 +0200

    Adds working server listener start and stop, accepts multiple socket connections

--- RobotClient/Models/StateObject.cs
+++ RobotClient/Models/StateObject.cs
@@ -5,13 +5,21 @@ namespace RobotClient.Models
 {
     public class StateObject
     {
+        // Id 
+        private int id;
         // Client  socket.  
-        public Socket workSocket = null;
+        public Socket listener = null;
         // Size of receive buffer.  
         public const int BufferSize = 1024;
         // Receive buffer.  
         public byte[] buffer = new byte[BufferSize];
         // Received data string.  
         public StringBuilder sb = new StringBuilder();
+
+        public int Id
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
     }
 }
