52fdcba045bf05bc58311fd0ad634a536ad7e9f3
Vid Cresnik <vcresnik@gmail.com>
2020-04-14 13:59:57 +0200

    Some small bug

--- RobotClient/Models/FocusModel.cs
+++ RobotClient/Models/FocusModel.cs
@@ -5,7 +5,7 @@ namespace RobotInterface.Models
     public class FocusModel
     {
         public string Name { get; set; }
-        public int Val { get; set; }
+        public double Val { get; set; }
         public int Idx { get; set; }
     }
 }
