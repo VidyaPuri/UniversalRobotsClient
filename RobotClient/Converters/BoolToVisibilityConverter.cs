bbfa03c75e9dcf24234364d48cbeb3bb7e800070
Gašper Mauer <gasper.mauer@gmail.com>
2020-04-24 17:01:41 +0200

    Added Converter folder and visibiliti options

---
+++ RobotClient/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RobotInterface.Converters
+{
+    class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var booleanVal = (bool)value;
+            if (booleanVal)
+                return Visibility.Visible;
+            else
+                return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
